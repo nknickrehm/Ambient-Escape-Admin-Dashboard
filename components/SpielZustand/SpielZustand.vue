7ac5aa4e939eaf4ed561b9384f3f0661d6996855
SalzstangeSalamiBrief <jeffrey.pluntke@student.uni-luebeck.de>
2019-11-29 15:30:13 +0100

    Refactor SpielZustands.vue

--- components/SpielZustand/SpielZustand.vue
+++ components/SpielZustand/SpielZustand.vue
@@ -4,61 +4,17 @@
     <button @click="stopGame" class="button is-danger">
       Spiel beenden
     </button> -->
-    <div class="tile is-ancestor">
-      <div class="tile is-parent">
-        <article class="tile is-child is-6">
-          <h1 class="tile__header">
-            Guppe A
-          </h1>
-          <progressbar :progress="70" />
-          <log :logs="log" />
-        </article>
-        <article class="tile is-child is-6">
-          <h1 class="tile__header">
-            Guppe B
-          </h1>
-          <progressbar :progress="70" />
-          <log :logs="log" />
-        </article>
-      </div>
-    </div>
-    <div class="tile is-ancestor">
-      <div class="tile is-parent">
-        <article class="tile is-child is-6">
-          <h1 class="tile__header">
-            Guppe C
-          </h1>
-          <progressbar :progress="70" />
-          <log :logs="log" />
-        </article>
-        <article class="tile is-child is-6">
-          <h1 class="tile__header">
-            Guppe D
-          </h1>
-          <progressbar :progress="70" />
-          <log :logs="log" />
-        </article>
-      </div>
-    </div>
+    <tile-group />
   </div>
 </template>
 
 <script>
 import GameState from '../../helpers/gamestate'
-import Progressbar from './Progressbar.vue'
-import Log from './Log.vue'
+import TileGroup from './TileGroup.vue'
 export default {
   name: 'SpielZustand',
   components: {
-    Progressbar,
-    Log
-  },
-  data () {
-    return {
-      log: [
-        { text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi urna justo, accumsan mollis imperdiet id', date: new Date() }, { text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi urna justo, accumsan mollis imperdiet id', date: new Date() }, { text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi urna justo, accumsan mollis imperdiet id', date: new Date() }, { text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi urna justo, accumsan mollis imperdiet id', date: new Date() }
-      ]
-    }
+    TileGroup
   },
   methods: {
     stopGame () {
---
+++ components/SpielZustand/TileGroup.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="tile is-ancestor">
+      <div class="tile is-parent">
+        <article class="tile is-child is-6">
+          <h1 class="tile__header">
+            Guppe A
+          </h1>
+          <progressbar :progress="70" />
+          <log :logs="log" />
+        </article>
+        <article class="tile is-child is-6">
+          <h1 class="tile__header">
+            Guppe B
+          </h1>
+          <progressbar :progress="70" />
+          <log :logs="log" />
+        </article>
+      </div>
+    </div>
+    <div class="tile is-ancestor">
+      <div class="tile is-parent">
+        <article class="tile is-child is-6">
+          <h1 class="tile__header">
+            Guppe C
+          </h1>
+          <progressbar :progress="70" />
+          <log :logs="log" />
+        </article>
+        <article class="tile is-child is-6">
+          <h1 class="tile__header">
+            Guppe D
+          </h1>
+          <progressbar :progress="70" />
+          <log :logs="log" />
+        </article>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Progressbar from './Progressbar.vue'
+import Log from './Log.vue'
+
+export default {
+  name: 'TileGroup',
+  components: {
+    Progressbar,
+    Log
+  },
+  data () {
+    return {
+      log: [
+        { text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi urna justo, accumsan mollis imperdiet id', date: new Date() }, { text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi urna justo, accumsan mollis imperdiet id', date: new Date() }, { text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi urna justo, accumsan mollis imperdiet id', date: new Date() }, { text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi urna justo, accumsan mollis imperdiet id', date: new Date() }
+      ]
+    }
+  }
+}
+
+</script>
