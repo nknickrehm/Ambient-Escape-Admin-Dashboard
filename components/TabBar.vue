614a72b2a752f68ffced92e1ebdc62cb92b97c0c
Jeffrey Pluntke <jeffrey.pluntke@student.uni-luebeck.de>
2019-12-14 12:24:54 +0100

    Remove disabled

--- components/TabBar.vue
+++ components/TabBar.vue
@@ -2,7 +2,7 @@
   <div class="tabs is-centered is-boxed">
     <ul>
       <li v-for="(tab, index) in tabs" :key="index" :class="tab.isActive ? 'is-active' : ''">
-        <a @click="$emit('change', index)" :class="{disabled: (isGameStarted && tab.label === 'Start') || (!isGameStarted && tab.label !== 'Start')}">
+        <a @click="$emit('change', index)">
           <span class="icon is-small">
             <i :class="`fas fa-${tab.icon}`" aria-hidden="true" />
           </span>
@@ -21,17 +21,9 @@ export default {
       type: Array,
       default () { return [] }
     }
-  },
-  computed: {
-    isGameStarted () {
-      return (this.$store.state.gamestate.currentState === 1)
-    }
   }
 }
 </script>
 
 <style scoped>
-.disabled{
-  pointer-events: none;
-}
 </style>
