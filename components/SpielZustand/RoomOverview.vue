1c8c03d4c6c3dc78e7a5c4411993141ff23c001c
Jeffrey Pluntke <jeffrey.pluntke@student.uni-luebeck.de>
2019-12-15 13:38:10 +0100

    Merge with other branches

--- components/SpielZustand/RoomOverview.vue
+++ components/SpielZustand/RoomOverview.vue
@@ -1,47 +1,51 @@
 <template>
-    <div class="roomWrapper">
-        <div class="mapWrapper">
-            <svg width="100%" height="700" xmlns="http://www.w3.org/2000/svg">
-                    <rect
-                    height="500"
-                    width="500"
-                    y="100"
-                    x="400"
-                    stroke-width="0"
-                    fill="#dedede"/>
+  <div class="roomWrapper">
+    <div class="mapWrapper">
+      <svg width="100%" height="700" xmlns="http://www.w3.org/2000/svg">
+        <rect
+          height="500"
+          width="500"
+          y="100"
+          x="400"
+          stroke-width="0"
+          fill="#dedede"
+        />
 
-                    <rect
-                    height="120"
-                    width="250"
-                    y="300"
-                    x="640"
-                    stroke-width="0"
-                    fill="#000000"/>
+        <rect
+          height="120"
+          width="250"
+          y="300"
+          x="640"
+          stroke-width="0"
+          fill="#000000"
+        />
 
-                    <g :visibility="element1">
-                        <line
-                        x1="770"
-                        y1="350"
-                        x2="950"
-                        y2="350"
-                        style="stroke:rgb(255,0,0);stroke-width:2" />
+        <g :visibility="element1">
+          <line
+            x1="770"
+            y1="350"
+            x2="950"
+            y2="350"
+            style="stroke:rgb(255,0,0);stroke-width:2"
+          />
 
-                        <text x="960" y="350" fill="black">Gruppe 1: Schlüssel 1</text>
-                    </g>
+          <text x="960" y="350" fill="black">Gruppe 1: Schlüssel 1</text>
+        </g>
 
-                    <ellipse
-                    ry="20"
-                    rx="20"
-                    cy="350"
-                    cx="770"
-                    :opacity="element1O"
-                    stroke-width="0"
-                    @mouseenter="element1O = .7; element1='show'"
-                    @mouseleave="element1O = 1; element1='hidden'"
-                    fill="red"/>
-                </svg>
-        </div>
+        <ellipse
+          :opacity="element1O"
+          @mouseenter="element1O = .7; element1='show'"
+          @mouseleave="element1O = 1; element1='hidden'"
+          ry="20"
+          rx="20"
+          cy="350"
+          cx="770"
+          stroke-width="0"
+          fill="red"
+        />
+      </svg>
     </div>
+  </div>
 </template>
 
 <script>
